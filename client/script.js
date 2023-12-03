b962a3871745ff2cfc8bb539346cef38264903bb
SuDy0906 <127987188+SuDy0906@users.noreply.github.com>
2023-12-03 15:53:19 +0530

    second commit server url

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://openai-ijjm.onrender.com/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
